405403c25b168e7ca13cbe940ca496b32dfd4943
wilzoka <wil.lb@hotmail.com>
2018-02-05 16:37:30 -0200

    Fix

--- routes/event.js
+++ routes/event.js
@@ -8,7 +8,7 @@ module.exports = function (app) {
         try {
             let viewevent = await db.getModel('viewevent').find({ where: { id: req.params.id }, include: { all: true } });
             let config = await db.getModel('config').find();
-            let custom = reload('../custom/' + config.customfile);
+            let custom = require('../custom/' + config.customfile);
             let realfunction = application.functions.getRealReference(custom, viewevent.function);
             let ids = [];
             if (req.query.ids) {
