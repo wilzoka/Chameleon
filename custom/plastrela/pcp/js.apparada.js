73c4b4455cd9e96715ce0a909698d4f6bd0851a8
wilzoka <wil.lb@hotmail.com>
2018-03-29 10:34:35 -0300

    Fixes

--- custom/plastrela/pcp/js.apparada.js
+++ custom/plastrela/pcp/js.apparada.js
@@ -8,7 +8,20 @@ $(function () {
             }
         });
 
+        application.jsfunction('plastrela.pcp.ap.js_usuarioUltimoAp', {
+            idoprecurso: application.functions.getUrlParameter('parent')
+        }, function (response) {
+            if (response.data.id) {
+                var newOption = new Option(response.data.text, response.data.id, false, false);
+                $('select[name="iduser"]').append(newOption).trigger('change');
+            }
+        });
+
         $('input[name="datafim"]').val(moment().format('DD/MM/YYYY HH:mm'));
+
+        setTimeout(function () {
+            $('select[name="idmotivoparada"').focus();
+        }, 100);
 
     }
 
--- custom/plastrela/pcp/js.apperda.js
+++ custom/plastrela/pcp/js.apperda.js
@@ -1,5 +1,24 @@
 $(function () {
 
+    if (application.functions.getId() == 0) {
+
+        application.jsfunction('plastrela.pcp.ap.js_usuarioUltimoAp', {
+            idoprecurso: application.functions.getUrlParameter('parent')
+        }, function (response) {
+            if (response.data.id) {
+                var newOption = new Option(response.data.text, response.data.id, false, false);
+                $('select[name="iduser"]').append(newOption).trigger('change');
+            }
+        });
+
+        $('input[name="datahora"]').val(moment().format('DD/MM/YYYY HH:mm'));
+
+        setTimeout(function () {
+            $('select[name="idtipoperda"').focus();
+        }, 100);
+
+    }
+
     var $idtipoperda = $('select[name="idtipoperda"]');
     var $idetapacausa = $('select[name="idetapacausa"]');
 
