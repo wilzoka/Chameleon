30392bcd13a93ff5a4b248b64409e2df29ae4b18
wilzoka <wil.lb@hotmail.com>
2018-12-26 08:22:27 -0200

    Fix

--- custom/platform.js
+++ custom/platform.js
@@ -1856,10 +1856,10 @@ let platform = {
                     let pdfMakePrinter = require('pdfmake');
                     let fontDescriptors = {
                         Roboto: {
-                            normal: 'fonts/cour.ttf',
-                            bold: 'fonts/courbd.ttf',
-                            italics: 'fonts/couri.ttf',
-                            bolditalics: 'fonts/courbi.ttf'
+                            normal: __dirname + '/../fonts/cour.ttf',
+                            bold: __dirname + '/../fonts/courbd.ttf',
+                            italics: __dirname + '/../fonts/couri.ttf',
+                            bolditalics: __dirname + '/../fonts/courbi.ttf'
                         }
                     };
                     let printer = new pdfMakePrinter(fontDescriptors);
