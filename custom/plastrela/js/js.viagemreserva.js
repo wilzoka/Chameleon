da7d89cbd4738958bc0faaf3676ea8dc6fe15fc8
wilzoka <wil.lb@hotmail.com>
2019-03-21 16:55:28 -0300

    Fix

--- custom/plastrela/js/js.viagemreserva.js
+++ custom/plastrela/js/js.viagemreserva.js
@@ -3,43 +3,43 @@ $(function () {
     function translate() {
         switch ($('input[name="tipo"]:checked').val()) {
             case 'Passagem Aérea':
-                $('input[name="local1"]').siblings('label').html('Origem');
-                $('input[name="data1"]').siblings('label').html('Data Embarque');
-                $('input[name="hora1"]').siblings('label').html('Hora Embarque');
+                $('input[name="local1"]').siblings('label').html('Origem*');
+                $('input[name="data1"]').siblings('label').html('Data Embarque*');
+                $('input[name="hora1"]').siblings('label').html('Hora Embarque*');
 
                 $('input[name="local2"]').siblings('label').parent().parent().removeClass('hidden');
-                $('input[name="local2"]').siblings('label').html('Destino');
-                $('input[name="data2"]').siblings('label').html('Data Chegada');
-                $('input[name="hora2"]').siblings('label').html('Hora Chegada');
+                $('input[name="local2"]').siblings('label').html('Destino*');
+                $('input[name="data2"]').siblings('label').html('Data Chegada*');
+                $('input[name="hora2"]').siblings('label').html('Hora Chegada*');
                 break;
             case 'Hospedagem':
-                $('input[name="local1"]').siblings('label').html('Local');
-                $('input[name="data1"]').siblings('label').html('Data Check-in');
-                $('input[name="hora1"]').siblings('label').html('Check-in');
+                $('input[name="local1"]').siblings('label').html('Local*');
+                $('input[name="data1"]').siblings('label').html('Data Check-in*');
+                $('input[name="hora1"]').siblings('label').html('Check-in*');
 
                 $('input[name="local2"]').siblings('label').parent().parent().addClass('hidden');
-                $('input[name="data2"]').siblings('label').html('Data Check-out');
-                $('input[name="hora2"]').siblings('label').html('Hora Check-out');
+                $('input[name="data2"]').siblings('label').html('Data Check-out*');
+                $('input[name="hora2"]').siblings('label').html('Hora Check-out*');
                 break;
             case 'Aluguel Carro':
-                $('input[name="local1"]').siblings('label').html('Local Retirada');
-                $('input[name="data1"]').siblings('label').html('Data Retirada');
-                $('input[name="hora1"]').siblings('label').html('Hora Retirada');
+                $('input[name="local1"]').siblings('label').html('Local Retirada*');
+                $('input[name="data1"]').siblings('label').html('Data Retirada*');
+                $('input[name="hora1"]').siblings('label').html('Hora Retirada*');
 
                 $('input[name="local2"]').siblings('label').parent().parent().removeClass('hidden');
-                $('input[name="local2"]').siblings('label').html('Local Devolução');
-                $('input[name="data2"]').siblings('label').html('Data Devolução');
-                $('input[name="hora2"]').siblings('label').html('Hora Devolução');
+                $('input[name="local2"]').siblings('label').html('Local Devolução*');
+                $('input[name="data2"]').siblings('label').html('Data Devolução*');
+                $('input[name="hora2"]').siblings('label').html('Hora Devolução*');
                 break;
             case 'Translado':
-                $('input[name="local1"]').siblings('label').html('Local Origem');
-                $('input[name="data1"]').siblings('label').html('Data Partida');
-                $('input[name="hora1"]').siblings('label').html('Hora Partida');
+                $('input[name="local1"]').siblings('label').html('Local Origem*');
+                $('input[name="data1"]').siblings('label').html('Data Partida*');
+                $('input[name="hora1"]').siblings('label').html('Hora Partida*');
 
                 $('input[name="local2"]').siblings('label').parent().parent().removeClass('hidden');
-                $('input[name="local2"]').siblings('label').html('Local Destino');
-                $('input[name="data2"]').siblings('label').html('Data Chegada');
-                $('input[name="hora2"]').siblings('label').html('Hora Chegada');
+                $('input[name="local2"]').siblings('label').html('Local Destino*');
+                $('input[name="data2"]').siblings('label').html('Data Chegada*');
+                $('input[name="hora2"]').siblings('label').html('Hora Chegada*');
                 break;
         }
     }
