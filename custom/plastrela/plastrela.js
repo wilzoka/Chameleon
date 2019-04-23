b65741df7cb97ce1d98fa37d85f50ae64fa669d5
wilzoka <wil.lb@hotmail.com>
2019-04-23 14:51:11 -0300

    Fix

--- custom/plastrela/plastrela.js
+++ custom/plastrela/plastrela.js
@@ -8766,7 +8766,6 @@ let main = {
                         if (obj.ids.length != 1) {
                             return application.error(obj.res, { msg: application.message.selectOnlyOneEvent });
                         }
-
                         let param = await db.getModel('parameter').findOne({ where: { key: 'pcp_oprecurso_reintegrar' } });
                         if (!param) {
                             return application.error(obj.res, { msg: 'Parâmetro não configurado' });
@@ -8775,11 +8774,11 @@ let main = {
                         if (param.indexOf(obj.req.user.id) < 0) {
                             return application.error(obj.res, { msg: 'Você não tem permissão para realizar esta ação' });
                         }
-
-                        let oprecurso = await db.getModel('pcp_oprecurso').findOne({ include: [{ all: true }], where: { id: obj.ids[0] } });
-                        if (oprecurso.pcp_opestado.descricao != 'Encerrada') {
-                            return application.error(obj.res, { msg: 'Só é possível reintegrar OPs Encerradas' });
-                        }
+                        await db.getModel('pcp_apinsumo').update({ integrado: false }, { where: { idoprecurso: obj.ids[0] } });
+                        await db.getModel('pcp_apperda').update({ integrado: false }, { where: { idoprecurso: obj.ids[0] } });
+                        await db.getModel('pcp_apparada').update({ integrado: false }, { where: { idoprecurso: obj.ids[0] } });
+                        await db.getModel('pcp_approducao').update({ integrado: false }, { where: { idoprecurso: obj.ids[0] } });
+                        let oprecurso = await db.getModel('pcp_oprecurso').findOne({ include: [{ all: true }], where: { id: obj.ids[0] } });                        
                         oprecurso.integrado = 'P';
                         await oprecurso.save({ iduser: obj.req.user.id });
                         return application.success(obj.res, { msg: application.message.success });
