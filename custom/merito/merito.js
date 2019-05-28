30130aedb6f356b2b2b12711251f9218dafc4617
wilzoka <wil.lb@hotmail.com>
2019-05-27 21:14:54 -0300

    Fix

--- custom/merito/merito.js
+++ custom/merito/merito.js
@@ -99,11 +99,11 @@ let main = {
                 let eventos = await db.getModel('eve_evento').findAll({ where: { publicar: true } });
                 let data = [];
                 for (let i = 0; i < eventos.length; i++) {
-                    let fotocapa = JSON.parse(eventos[i].fotocapa || '[{}]')[0];
+                    let fotocapa = JSON.parse(eventos[i].fotocapa || '[]')[0];
                     let fotopublicar = JSON.parse(eventos[i].fotopublicar || '[]');
                     let fp = [];
                     for (let z = 0; z < fotopublicar.length; z++) {
-                        fp.push(`${fotopublicar[z].id}.${fotopublicar[z].type}`)
+                        fp.push(`${fotopublicar[z].id}`)
                     }
                     data.push({
                         id: eventos[i].id
