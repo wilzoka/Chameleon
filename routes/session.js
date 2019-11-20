8c078fdf3e9e3d591be37b51d17510b8c99b74a3
wilzoka <wil.lb@hotmail.com>
2019-11-20 08:37:31 -0300

    Fix

--- routes/session.js
+++ routes/session.js
@@ -98,6 +98,7 @@ module.exports = function(app) {
 
     app.get('/logout', function(req, res) {
         req.logout();
+        req.session.destroy();
         return res.redirect("/login");
     });
 
