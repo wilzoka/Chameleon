2da20382f54dd499a7c917e87af5f6660fced457
William Bach <williamb@plastrela.com.br>
2021-03-02 13:22:56 -0300

    Most eficient autocomplete fill

--- routes/autocomplete.js
+++ routes/autocomplete.js
@@ -6,22 +6,20 @@ module.exports = function (app) {
 
     app.get('/autocomplete', application.IsAuthenticated, async (req, res) => {
         try {
-            const field = req.query.query || req.query.attribute;
-            const where = [];
-            if (req.query.where) {
-                where.push(req.query.where);
-            }
-            if (req.query.q) {
-                where.push(field + "::text ilike '%" + req.query.q + "%'");
-            }
-            let wherestr = '';
-            if (where.length > 0) {
-                wherestr = 'where ' + where.join(' and ');
-                wherestr = wherestr.replace(/\$user/g, req.user.id);
-            }
-            const query = `SELECT id, ${field} as text FROM ${req.query.model} ${wherestr} ORDER BY ${field} LIMIT 100`;
-            const results = await db.sequelize.query(query, { type: db.sequelize.QueryTypes.SELECT });
-            application.success(res, { data: results });
+            const f = req.query.query || req.query.attribute;
+            const q = req.query.q || '';
+            const m = req.query.model;
+            const w = (`where ${req.query.where || '1=1'}`)
+                .replace(/\$user/g, req.user.id);
+            const query = `
+            (SELECT id, ${f} as text FROM ${m} ${w} and ${f}::text ilike '${q}' ORDER BY ${f})
+            union all
+            (SELECT id, ${f} as text FROM ${m} ${w} and ${f}::text ilike '${q}%' and ${f}::text not ilike '${q}' ORDER BY ${f} LIMIT 100)
+            union all
+            (SELECT id, ${f} as text FROM ${m} ${w} and ${f}::text ilike '%${q}%' and ${f}::text not ilike '${q}' and ${f}::text not ilike '${q}%' ORDER BY ${f} LIMIT 100)
+            `;
+            const r = await db.query(query);
+            application.success(res, { data: r });
         } catch (err) {
             application.fatal(res, err);
         }
